f7ce36494e1147847766d39469dafad4bae2d121
0xelia <elia.vanon@yahoo.com>
2022-09-26 15:37:07 +0200

    methods

--- api/movies.php
+++ api/movies.php
@@ -5,15 +5,22 @@ class Movie {
     public $lang;
     public $year;
     public $poster = null;
+    public $actors = 0;
     public $cast = [];
 
+    
     function __construct($title, $lang, $poster, $year, $cast = []){
         $this->title = $title;
         $this->lang = $lang;
         $this->poster = $poster;
         $this->year = $year;
-        $this->cast = $cast;
+        $this->cast = [
+            'actors' => numOfActors($cast),
+            'cast' => $cast
+        ];
     }
-
-    function 
+    
+    public function numOfActors($cast){
+        return count($cast);
+    }
 }
--- index.php
+++ index.php
@@ -19,7 +19,7 @@ $cast_movie_2 =[
 
 
 $movie_1 = new Movie ('Pirates of Caribbean', 'US', 'https://m.media-amazon.com/images/M/MV5BNGYyZGM5MGMtYTY2Ni00M2Y1LWIzNjQtYWUzM2VlNGVhMDNhXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_UX67_CR0,0,67,98_AL_.jpg' , 2003, $cast_movie_1);
-$movie_2 = new Movie ('Spider Man', 'US', null, 2002, $cast_movie_2);
+$movie_2 = new Movie ('Spider Man', 'US', 'https://imgs.search.brave.com/ZPmDgcIkBqajmkXmN56tq8lvhiXy9Mza4caDSh5wWtw/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly9pbWFn/ZS50bWRiLm9yZy90/L3Avb3JpZ2luYWwv/OFlON2RZbUpoVnZO/NmtPZ0JBTmxtb05p/WXNkLmpwZw', 2002, $cast_movie_2);
 
 var_dump($movie_1, $movie_2);
 
